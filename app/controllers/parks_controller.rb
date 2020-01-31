47c0940ac9b423302f719772fcc7c3a66c72c6c5
Joshua Forman <cyborgforman@gmail.com>
2020-01-31 11:34:53 -0800

    update parks controller

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,12 +1,41 @@
-class QuotesController < ApplicationController
+class ParksController < ApplicationController
 
   def index
-    @quotes = {"quotation": "The secret of getting ahead is getting started."}
-    json_response(@quotes)
+    @parks = Park.all
+    json_response(@parks)
+  end
+
+  def show
+    @park = Park.find(params[:id])
+    json_response(@park)
+  end
+
+  def create
+    @park = Park.create!(park_params)
+    json_response(@park, :created)
+  end
+
+  def update
+    @park = Park.find(params[:id])
+    if @park.update!(park_params)
+      render status: 200, json: {
+        message: "Updated Park Successfully."
+      }
+    end
+  end
+
+  def destroy
+    @park = Park.find(params[:id])
+    if @park.destroy!
+      render status: 200, json: {
+        message: "DESTROYED Park successfully."
+      }
+    end
   end
 
   private
-  def json_response(object, status = :ok)
-    render json: object, status: status
+  def park_params
+    params.permit(:name, :state)
   end
+
 end
