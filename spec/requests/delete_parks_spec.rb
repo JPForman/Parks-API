3153b3ca2c500276c53d01d887ecaee1e25bee1d
Joshua Forman <cyborgforman@gmail.com>
2020-01-31 17:20:32 -0800

    update delete spec

--- spec/requests/delete_parks_spec.rb
+++ spec/requests/delete_parks_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 require 'pry'
 
-describe "post parks route", :type => :request do
+describe "delete park route", :type => :request do
 
   before do
     park = Park.all.sample
-    patch "/parks/#{park.id}", params: { :name => 'Cool Dude', :state => 'chill'}
+    delete "/parks/#{park.id}"
   end
 
 
   it 'updates park successfully' do
-    expect(JSON.parse(response.body)).to eq("message" => "Updated Park Successfully.")
+    expect(JSON.parse(response.body)).to eq("message"=>"DESTROYED Park successfully.")
   end
 
   it 'returns status code 200' do
